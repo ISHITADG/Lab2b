5e129a6a739050c6c2b47b95634e8603be8817d1
Ishita Dasgupta <ishitadg@cs.umass.edu>
2020-11-10 07:06:11 -0500

    mininet topo for Lab 2b

--- lab2btopo.py
+++ lab2btopo.py
@@ -3,6 +3,8 @@
 One switch with server and client on either side:
 
    host --- switch --- host
+               |
+              host
 
 Adding the 'topos' dict with a key/value pair to generate our newly defined
 topology enables one to pass in '--topo=mytopo' from the command line.
@@ -20,15 +22,13 @@ class MyTopo( Topo ):
         Topo.__init__( self )
 
         # Add hosts and switches
-        server = self.addHost('PC2', ip='10.10.1.1/24' )
-        client = self.addHost('PC1', ip='10.10.3.2/24' )
-        switch1 = self.addSwitch( 's1' )
-        switch2 = self.addSwitch( 's2' )
+        # 653: ADD HOSTS HERE
+        switch = self.addSwitch( 's3' )
 
-        # Add links
-        self.addLink( client, switch2 )
-        self.addLink( switch2, switch1 )
-        self.addLink( switch1, server )
+        # Add links (DO NOT MODIFY OR CHANGE THIS ORDER BELOW)
+        self.addLink( h1, switch )
+        self.addLink( h2, switch )
+        self.addLink( switch, h3 )
 
 
 topos = { 'mytopo': ( lambda: MyTopo() ) }
